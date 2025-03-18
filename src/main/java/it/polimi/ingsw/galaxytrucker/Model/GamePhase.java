ffb65fc64b058d2009d6301f2fbf5145e0c81740
Luca Papiro <luca.papiro10@gmail.com>
2025-03-18 11:03:35 +0100

    added Gamephase and Hourglass

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePhase.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePhase.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.galaxytrucker.Model;
 
 public enum GamePhase {
+    WAITING_ROOM, BUILDING, TRAVELLING;
 }
--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Hourglass.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Hourglass.java
@@ -1,4 +1,40 @@
 package it.polimi.ingsw.galaxytrucker.Model;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+//the Timer class implementation is kept (in comments) for possible future use
 public class Hourglass {
+    private int FlipNumber = 0;
+//    private Boolean hasEnded;
+//    private Timer timer = new Timer();
+    private long startTime;
+
+    public void flip(){
+//        hasEnded = false;
+        startTime = System.currentTimeMillis();
+        FlipNumber++;
+        //task that is executed when 60 seconds have passed
+/*        TimerTask task = new TimerTask() {
+            public void run() {
+                hasEnded = true;
+                timer.cancel();
+            }
+        };
+        timer.schedule(task, 60000);
+*/
+    }
+
+    public int getFlipNumber() {
+        return FlipNumber;
+    }
+
+/*    public Boolean getHasEnded() {
+        return hasEnded;
+    }
+*/
+    public long getElapsedTime(){
+        return System.currentTimeMillis() - startTime;
+    }
+
 }
