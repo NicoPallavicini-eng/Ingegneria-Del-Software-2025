9ee48cdeed079bcef4af220c5dc7073c962a03c2
elisa <elisa.garsetti@mail.polimi.it>
2025-04-01 12:31:17 +0200

    added proper removal of tiles from ship, added a projectile util for that in ProjectileUtils

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Cannonball.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Cannonball.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.galaxytrucker.Model.Cards;
 
 import it.polimi.ingsw.galaxytrucker.Model.Direction;
 import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Ship;
-import it.polimi.ingsw.galaxytrucker.Model.Tiles.ShieldOrientation;
 import it.polimi.ingsw.galaxytrucker.Model.Tiles.ShieldTile;
 
 import java.util.ArrayList;
@@ -31,139 +30,49 @@ public record Cannonball(boolean bigCannonball, Direction direction, RowOrColumn
 
         if (this.rowOrColumn == ROW) {
             if (diceRoll >= 5 && diceRoll <= 9 && !ship.getRowListTiles(diceRoll).isEmpty()) {
-                checkForShieldOrRemove(ship, diceRoll, SOUTHWEST, WEST, NORTHEAST, EAST);
+                checkForShieldOrRemoveRow(ship, diceRoll);
             }
         } else if (this.rowOrColumn == COLUMN) {
             if (diceRoll >= 4 && diceRoll <= 10 && !ship.getColumnListTiles(diceRoll).isEmpty()) {
-                checkForShieldOrRemove(ship, diceRoll, NORTHEAST, NORTH, SOUTHWEST, SOUTH);
+                checkForShieldOrRemoveColumn(ship, diceRoll);
             }
         }
     }
 
-    private void checkForShieldOrRemove(Ship ship, int diceRoll, ShieldOrientation shieldOrientation, Direction direction, ShieldOrientation shieldOrientation2, Direction direction2) {
+    private void checkForShieldOrRemoveRow(Ship ship, int diceRoll) {
         if (this.bigCannonball) {
-            removeHitTile(ship, diceRoll);
+            ProjectileUtils.removeHitTileRow(this.direction, ship, diceRoll);
         } else {
-            ArrayList<ShieldTile> shields = ship.getListOfShield();
+            ArrayList <ShieldTile> shields = ship.getListOfShield();
             boolean hasShield = false;
             for (ShieldTile shield : shields) {
-                if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == shieldOrientation) && this.direction == direction
-                        || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == shieldOrientation2) && this.direction == direction2) {
+                if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == SOUTHWEST) && this.direction == WEST
+                        || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == NORTHEAST) && this.direction == EAST) {
                     hasShield = true;
                     break;
                 }
             }
             if (!hasShield) {
-                removeHitTile(ship, diceRoll);
+                ProjectileUtils.removeHitTileRow(this.direction, ship, diceRoll);
             }
         }
     }
 
-    private void removeHitTile(Ship ship, int diceRoll) {
-        int rowSize = ship.getRowListTiles(diceRoll).size();
-        if (this.direction == WEST) {
-            if (rowSize == 5) {
-                ship.removeTileOnFloorPlan(diceRoll, 9);
-            } else if (rowSize == 4) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    ship.removeTileOnFloorPlan(diceRoll, 8);
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 3) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        ship.removeTileOnFloorPlan(diceRoll, 7);
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 2) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            ship.removeTileOnFloorPlan(diceRoll, 6);
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 1) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            if (ship.getRowListTiles(6).isEmpty()) {
-                                ship.removeTileOnFloorPlan(diceRoll, 5);
-                            } else {
-                                ship.removeTileOnFloorPlan(diceRoll, 6);
-                            }
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
+    private void checkForShieldOrRemoveColumn(Ship ship, int diceRoll) {
+        if (this.bigCannonball) {
+            ProjectileUtils.removeHitTileColumn(this.direction, ship, diceRoll);
+        } else {
+            ArrayList <ShieldTile> shields = ship.getListOfShield();
+            boolean hasShield = false;
+            for (ShieldTile shield : shields) {
+                if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == NORTHEAST) && this.direction == NORTH
+                        || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == SOUTHWEST) && this.direction == SOUTH) {
+                    hasShield = true;
+                    break;
                 }
             }
-        } else if (this.direction == EAST) {
-            if (rowSize == 5) {
-                ship.removeTileOnFloorPlan(diceRoll, 5);
-            } else if (rowSize == 4) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    ship.removeTileOnFloorPlan(diceRoll, 4);
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
-            } else if (rowSize == 3) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        ship.removeTileOnFloorPlan(diceRoll, 7);
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
-            } else if (rowSize == 2) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            ship.removeTileOnFloorPlan(diceRoll, 8);
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
-            } else if (rowSize == 1) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            if (ship.getRowListTiles(8).isEmpty()) {
-                                ship.removeTileOnFloorPlan(diceRoll, 9);
-                            } else {
-                                ship.removeTileOnFloorPlan(diceRoll, 8);
-                            }
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
+            if (!hasShield) {
+                ProjectileUtils.removeHitTileColumn(this.direction, ship, diceRoll);
             }
         }
     }
--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Meteor.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Meteor.java
@@ -2,9 +2,13 @@ package it.polimi.ingsw.galaxytrucker.Model.Cards;
 
 import it.polimi.ingsw.galaxytrucker.Model.Direction;
 import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Ship;
+import it.polimi.ingsw.galaxytrucker.Model.Tiles.ConnectorType;
+import it.polimi.ingsw.galaxytrucker.Model.Tiles.EngineTile;
 import it.polimi.ingsw.galaxytrucker.Model.Tiles.ShieldTile;
+import it.polimi.ingsw.galaxytrucker.Model.Tiles.Tile;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static it.polimi.ingsw.galaxytrucker.Model.Cards.RowOrColumn.COLUMN;
@@ -12,28 +16,7 @@ import static it.polimi.ingsw.galaxytrucker.Model.Cards.RowOrColumn.ROW;
 import static it.polimi.ingsw.galaxytrucker.Model.Direction.*;
 import static it.polimi.ingsw.galaxytrucker.Model.Tiles.ShieldOrientation.*;
 
-public class Meteor {
-    private final boolean bigMeteor;
-    private final Direction direction;
-    private final RowOrColumn rowOrColumn;
-
-    public Meteor(boolean bigMeteor, Direction direction, RowOrColumn rowOrColumn) {
-        this.bigMeteor = bigMeteor;
-        this.direction = direction;
-        this.rowOrColumn = rowOrColumn;
-    }
-
-    public boolean isBigMeteor() {
-        return bigMeteor;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public RowOrColumn getRowOrColumn() {
-        return rowOrColumn;
-    }
+public record Meteor(boolean bigMeteor, Direction direction, RowOrColumn rowOrColumn) {
 
     public static int rollTwoDice() {
         Random rand = new Random();
@@ -51,153 +34,84 @@ public class Meteor {
 
         if (this.rowOrColumn == ROW) {
             if (diceRoll >= 5 && diceRoll <= 9 && !ship.getRowListTiles(diceRoll).isEmpty()) {
-                if (this.bigMeteor) {
-                    if (/* first tile or adjacent ones =! cannon */) {
-                        removeHitTile(ship, diceRoll);
-                    }
-                } else {
-                    ArrayList <ShieldTile> shields = ship.getListOfShield();
-                    boolean hasShield = false;
-                    for (ShieldTile shield : shields) {
-                        if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == SOUTHWEST) && this.direction == WEST
-                                || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == NORTHEAST) && this.direction == EAST) {
-                            hasShield = true;
-                            break;
-                        }
-                    }
-                    if (/* open connector || */ !hasShield) {
-                        removeHitTile(ship, diceRoll);
-                    }
-                }
+                checkForCannonOrRemoveRow(ship, diceRoll);
             }
         } else if (this.rowOrColumn == COLUMN) {
             if (diceRoll >= 4 && diceRoll <= 10 && !ship.getColumnListTiles(diceRoll).isEmpty()) {
-                if (this.bigMeteor) {
-                    if (/* first tile =! cannon */) {
-                        removeHitTile(ship, diceRoll);
-                    }
-                } else {
-                    ArrayList <ShieldTile> shields = ship.getListOfShield();
-                    boolean hasShield = false;
-                    for (ShieldTile shield : shields) {
-                        if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == NORTHEAST) && this.direction == NORTH
-                                || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == SOUTHWEST) && this.direction == SOUTH) {
-                            hasShield = true;
-                            break;
-                        }
-                    }
-                    if (/* open connector || */ !hasShield) {
-                        removeHitTile(ship, diceRoll);
-                    }
+                checkForCannonOrRemoveColumn(ship, diceRoll);
+            }
+        }
+    }
+
+    public void checkForCannonOrRemoveRow(Ship ship, int diceRoll) {
+        if (this.bigMeteor) {
+            List<EngineTile> engineTileList = ship.getListOfEngine();
+            Tile firstTile = ship.getRowListTiles(diceRoll).getFirst();
+            if (!engineTileList.contains(firstTile)) {
+                ProjectileUtils.removeHitTileRow(this.direction, ship, diceRoll);
+            }
+        } else {
+            ArrayList<ShieldTile> shields = ship.getListOfShield();
+            boolean hasShield = false;
+            for (ShieldTile shield : shields) {
+                if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == SOUTHWEST) && this.direction == WEST
+                        || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == NORTHEAST) && this.direction == EAST) {
+                    hasShield = true;
+                    break;
+                }
+            }
+            Tile firstTile = ship.getRowListTiles(diceRoll).getFirst();
+            if (this.direction == WEST) {
+                if ((firstTile.getConnectors().get(1) != ConnectorType.NONE
+                        || firstTile.getConnectors().get(1) != ConnectorType.CANNON
+                        || firstTile.getConnectors().get(1) != ConnectorType.ENGINE)
+                        && !hasShield) {
+                    ProjectileUtils.removeHitTileRow(this.direction, ship, diceRoll);
+                }
+            } else if (this.direction == EAST) {
+                if ((firstTile.getConnectors().getLast() != ConnectorType.NONE
+                        || firstTile.getConnectors().getLast() != ConnectorType.CANNON
+                        || firstTile.getConnectors().getLast() != ConnectorType.ENGINE)
+                        && !hasShield) {
+                    ProjectileUtils.removeHitTileRow(this.direction, ship, diceRoll);
                 }
             }
         }
     }
 
-    private void removeHitTile(Ship ship, int diceRoll) {
-        int rowSize = ship.getRowListTiles(diceRoll).size();
-        if (this.direction == WEST) {
-            if (rowSize == 5) {
-                ship.removeTileOnFloorPlan(diceRoll, 9);
-            } else if (rowSize == 4) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    ship.removeTileOnFloorPlan(diceRoll, 8);
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 3) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        ship.removeTileOnFloorPlan(diceRoll, 7);
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 2) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            ship.removeTileOnFloorPlan(diceRoll, 6);
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
-                }
-            } else if (rowSize == 1) {
-                if (ship.getColumnListTiles(9).isEmpty()) {
-                    if (ship.getRowListTiles(8).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            if (ship.getRowListTiles(6).isEmpty()) {
-                                ship.removeTileOnFloorPlan(diceRoll, 5);
-                            } else {
-                                ship.removeTileOnFloorPlan(diceRoll, 6);
-                            }
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 8);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 9);
+    public void checkForCannonOrRemoveColumn(Ship ship, int diceRoll) {
+        if (this.bigMeteor) {
+            List<EngineTile> engineTileList = ship.getListOfEngine();
+            Tile firstTile = ship.getColumnListTiles(diceRoll).getFirst();
+            Tile firstLeft = ship.getColumnListTiles(diceRoll - 1).getFirst();
+            Tile firstRight = ship.getColumnListTiles(diceRoll + 1).getFirst();
+            if (!engineTileList.contains(firstTile) && !engineTileList.contains(firstLeft) && !engineTileList.contains(firstRight)) {
+                ProjectileUtils.removeHitTileColumn(this.direction, ship, diceRoll);
+            }
+        } else {
+            ArrayList<ShieldTile> shields = ship.getListOfShield();
+            boolean hasShield = false;
+            for (ShieldTile shield : shields) {
+                if ((shield.getOrientation() == NORTHWEST || shield.getOrientation() == NORTHEAST) && this.direction == NORTH
+                        || (shield.getOrientation() == SOUTHEAST || shield.getOrientation() == SOUTHWEST) && this.direction == SOUTH) {
+                    hasShield = true;
+                    break;
                 }
             }
-        } else if (this.direction == EAST) {
-            if (rowSize == 5) {
-                ship.removeTileOnFloorPlan(diceRoll, 5);
-            } else if (rowSize == 4) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    ship.removeTileOnFloorPlan(diceRoll, 4);
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
+            Tile firstTile = ship.getColumnListTiles(diceRoll).getFirst();
+            if (this.direction == NORTH) {
+                if ((firstTile.getConnectors().getFirst() != ConnectorType.NONE
+                        || firstTile.getConnectors().getFirst() != ConnectorType.CANNON
+                        || firstTile.getConnectors().getFirst() != ConnectorType.ENGINE)
+                        && !hasShield) {
+                    ProjectileUtils.removeHitTileColumn(this.direction, ship, diceRoll);
                 }
-            } else if (rowSize == 3) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        ship.removeTileOnFloorPlan(diceRoll, 7);
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
-            } else if (rowSize == 2) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            ship.removeTileOnFloorPlan(diceRoll, 8);
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
-                }
-            } else if (rowSize == 1) {
-                if (ship.getColumnListTiles(5).isEmpty()) {
-                    if (ship.getRowListTiles(6).isEmpty()) {
-                        if (ship.getColumnListTiles(7).isEmpty()) {
-                            if (ship.getRowListTiles(8).isEmpty()) {
-                                ship.removeTileOnFloorPlan(diceRoll, 9);
-                            } else {
-                                ship.removeTileOnFloorPlan(diceRoll, 8);
-                            }
-                        } else {
-                            ship.removeTileOnFloorPlan(diceRoll, 7);
-                        }
-                    } else {
-                        ship.removeTileOnFloorPlan(diceRoll, 6);
-                    }
-                } else {
-                    ship.removeTileOnFloorPlan(diceRoll, 5);
+            } else if (this.direction == SOUTH) {
+                if ((firstTile.getConnectors().get(2) != ConnectorType.NONE
+                        || firstTile.getConnectors().get(2) != ConnectorType.CANNON
+                        || firstTile.getConnectors().get(2) != ConnectorType.ENGINE)
+                        && !hasShield) {
+                    ProjectileUtils.removeHitTileColumn(this.direction, ship, diceRoll);
                 }
             }
         }
