16df69f861b6d57513ecfb1824263e377eb476c1
elisa <elisa.garsetti@mail.polimi.it>
2025-04-04 10:01:47 +0200

    switched to interface instead of abstract class for cardVisitor

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/CardVisitors/CardVisitor.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/CardVisitors/CardVisitor.java
@@ -1,53 +1,42 @@
 package it.polimi.ingsw.galaxytrucker.Model.Cards.CardVisitors;
 
-public abstract class CardVisitor {
-    public CardVisitor() {}
+import it.polimi.ingsw.galaxytrucker.Model.Cards.*;
+import it.polimi.ingsw.galaxytrucker.Model.Game.Game;
+import it.polimi.ingsw.galaxytrucker.Model.Game.GameState;
+import it.polimi.ingsw.galaxytrucker.Model.Game.SequentialTravellingState;
+import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Player;
+import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Ship;
+
+import java.util.List;
+
+public interface CardVisitor {
 
     // handle cards
-
-    public void handleStationCard() {}
-
-    public void handleShipCard() {}
-
-    public void handleOpenSpaceCard() {}
-
-    public void handleStardustCard() {}
-
-    public void handleEpidemicCard() {}
-
-    public void handleCombatZoneCard() {}
-
-    public void handlePlanetsCard() {}
-
-    public void handleSmugglersCard() {}
-
-    public void handleMeteorsCard() {}
-
-    public void handleSlaversCard() {}
-
-    public void handlePiratesCard() {}
+    public void handleStationCard(SequentialTravellingState state, StationCard stationCard, Player player);
+    public void handleShipCard(SequentialTravellingState state, ShipCard shipCard, Player player);
+    public void handleOpenSpaceCard(OpenSpaceCard openSpaceCard, List<Player> players);
+    public void handleStardustCard(StardustCard stardustCard, Ship ship);
+    public void handleEpidemicCard(EpidemicCard epidemicCard, Ship ship);
+    public void handleCombatZoneCardLessCrew(CombatZoneCard combatZoneCard, Ship ship);
+    public void handleCombatZoneCardLessEngine(CombatZoneCard combatZoneCard, Ship ship);
+    public void handleCombatZoneCardLessFirepower(CombatZoneCard combatZoneCard, Ship ship);
+    public void handlePlanetsCard(PlanetsCard planetsCard, List <Player> players);
+    public void handleSmugglersCard(SequentialTravellingState state, SmugglersCard smugglersCard, Player player);
+    public void handleMeteorsCard(MeteorsCard meteorsCard, Ship ship);
+    public void handleSlaversCard(SequentialTravellingState state, SlaversCard slaversCard, Player player);
+    public void handlePiratesCard(SequentialTravellingState state, PiratesCard piratesCard, Player player);
 
     // set next states
+    public void setNextStateCombatZoneCard(GameState state, Game game, CombatZoneCard card);
+    public void setNextStateEpidemicCard(GameState state, Game game, EpidemicCard card);
+    public void setNextStateMeteorsCard(GameState state, Game game, MeteorsCard card);
+    public void setNextStateOpenSpaceCard(GameState state, Game game, OpenSpaceCard card);
+    public void setNextStatePiratesCard(GameState state, Game game, PiratesCard card);
+    public void setNextStatePlanetsCard(GameState state, Game game, PlanetsCard card);
+    public void setNextStateShipCard(GameState state, Game game, ShipCard card);
+    public void setNextStateSlaversCard(GameState state, Game game, SlaversCard card);
+    public void setNextStateSmugglersCard(GameState state, Game game, SmugglersCard card);
+    public void setNextStateStardustCard(GameState state, Game game, StardustCard card);
+    public void setNextStateStationCard(GameState state, Game game, StationCard card);
 
-    public void setNextStateCombatZoneCard() {}
-
-    public void setNextStateEpidemicCard() {}
-
-    public void setNextStateMeteorsCard() {}
-
-    public void setNextStateOpenSpaceCard() {}
-
-    public void setNextStatePiratesCard() {}
-
-    public void setNextStatePlanetsCard() {}
-
-    public void setNextStateShipCard() {}
-
-    public void setNextStateSlaversCard() {}
-
-    public void setNextStateSmugglersCard() {}
-
-    public void setNextStateStardustCard() {}
-
-    public void setNextStateStationCard() {}
 }
