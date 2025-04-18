aed11863cb5dc0edb00119c61c0358464cc509b8
Luca Papiro <luca.papiro10@gmail.com>
2025-04-18 10:58:48 +0200

    changed card

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Card.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Cards/Card.java
@@ -1,9 +1,6 @@
 package it.polimi.ingsw.galaxytrucker.Model.Cards;
 
-import it.polimi.ingsw.galaxytrucker.Model.Cards.CardVisitors.CardVisitor;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.*;
-import it.polimi.ingsw.galaxytrucker.Model.GamePackage.AlternativeTravellingState;
-import it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameStates.GameState;
 import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Player;
 import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Ship;
 
@@ -11,13 +8,11 @@ import java.util.List;
 
 public abstract class Card {
     private final boolean levelTwo;
-    private final CardVisitor visitor;
     private boolean used;
 
-    public Card(boolean levelTwo, boolean used, CardVisitor visitor) {
+    public Card(boolean levelTwo, boolean used) {
         this.levelTwo = levelTwo;
         this.used = false;
-        this.visitor = visitor;
     }
 
     public boolean isUsed() {
@@ -34,15 +29,5 @@ public abstract class Card {
 
     public void process () {}
 
-    public void acceptCardVisitorParallel(ParallelTravellingState state, CardVisitor visitor, List <Ship> ships) {}
 
-    public void acceptCardVisitorSequential(SequentialTravellingState state, CardVisitor visitor, List <Player> players) {}
-
-    public void acceptCardVisitorAlternative(AlternativeTravellingState state, CardVisitor visitor, List <Ship> lessCrewShips, List <Ship> lessEngineShips, List <Ship> lessFirepowerShips) {}
-
-    public void acceptNextVisitor(GameState state, CardVisitor visitor, Game game) {}
-
-    public CardVisitor getCardVisitor() {
-        return visitor;
-    }
 }
