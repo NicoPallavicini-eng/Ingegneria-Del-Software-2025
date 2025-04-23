042b9b34b84332131223982615c56b35fd716969
Nicolò Pallavicini <nicolo.pallavicini@mail.polimi.it>
2025-04-23 22:39:50 +0200

    Still trying to figure out how to handle messages properly

--- src/main/java/it/polimi/ingsw/galaxytrucker/Controller/Server/ServerController.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Controller/Server/ServerController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.galaxytrucker.Controller.Server;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.Game;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameEvents.HelpEvent;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameEvents.IllegalEventException;
+import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Player;
 
 import java.util.*;
 import java.util.function.Consumer;
@@ -9,6 +10,7 @@ import java.util.function.Consumer;
 public class ServerController {
     private final Map<String, Runnable> commands;
     private final Game game;
+    private String nickname;
 
     public ServerController(Map<String, Consumer<List<String>>> commands) {
         this.commands = new HashMap<>();
@@ -49,57 +51,81 @@ public class ServerController {
                     System.out.println("Errore: /connect richiede due parametri");
                     return;
                 }
-                String nickname = params.get(0);
-                String game = params.get(1);
-                ConnectEvent event 
-            }
+                this.nickname = params.get(0);
+                String IP = params.get(1);
 
-        });
-
-        commands.put("/connect", params -> {
-
-        });
-
-     /*
-        commands.put("/help", params -> {
-            try{
-                HelpEvent event = HelpEvent.from(params);
+                ArrayList<Player> listOfPlayers = listOfPlayers.getListOfPlayers();
+                ArrayList<String> listOfPlayersString = new ArrayList<String>();
+                for (Player player : listOfPlayers){
+                    String nickPlayer = player.getNickname();
+                    listOfPlayersString.add(nickPlayer);
+                }
+                if (listOfPlayersString.contains(nickname)){
+                    System.out.println("Errore: nickname già presente. Sceglierne uno univoco");
+                    return;
+                }
+                ConnectEvent event = new ConnectEvent(nickname, IP);
             }
             catch (IllegalEventException e){
-                System.out.println("Errore: " +e.getMessage());
+                System.out.println("Errore: " + e.getMessage());
                 System.err.println(e.getMessage());
             }
         });
 
-        commands.put("/viewships", params ->{
+        commands.put("/disconnect", params -> {
             try{
-                ViewShipsEvent event = ViewShipsEvents.from(params);
+                if (!params.isEmpty()) {
+                    System.out.println("Errore: /disconnect non richiede parametri");
+                    throw new IllegalEventException("Errore: /disconnect non richiede parametri");
+                }
+
+                DisconnectEvent event = new DisconnectEvent(this.nickname);
             }
             catch (IllegalEventException e){
-                System.out.println("Errore" +e.getMessage());
-                System.err.println(e.getMessage())
+                System.out.println("Errore: " + e.getMessage());
+                System.err.println(e.getMessage());
             }
         });
 
-        commands.put("/viewleaderboard", params -> {
-           try{
-               ViewLeaderboardEvent event = ViewLeaderBoardEvent.from(params);
-           }
-           catch (IllegalEventException e){
-               System.out.println("Errore" +e.getMessage());
-               System.err.println(e.getMessage());
-           }
-        });
+        commands.put("/setnumberofplayers", params -> {});
 
-        commands.put("/connect", params -> {
-            try{
-                ConnectEvent event = ConnectEvent.from(params);
-            }
-            catch (IllegalEventException e){
-                System.out.println("Errore" +e.getMessage());
-                System.err.println(e.getMessage();
-            }
-        });
-*/
+        commands.put("/setcolor", params -> {});
+
+        commands.put("/pickuptile", params -> {});
+
+        commands.put("/rotatetile", params -> {});
+
+        commands.put("/putdowntile", params -> {});
+
+        commands.put("/placetile", params -> {});
+
+        commands.put("/reservetile", params -> {});
+
+        commands.put("/fliphourglass", params -> {});
+
+        commands.put("/setposition", params -> {});
+
+        commands.put("/pickupfromship", params -> {});
+
+        commands.put("/pickupreservedtile", params -> {});
+
+        commands.put("/acrivateengine", params -> {});
+
+        commands.put("/activatecannons", params -> {});
+
+        commands.put("/activateshields", params -> {});
+
+        commands.put("/removecargo", params -> {});
+
+        commands.put("/addcargo", params -> {});
+
+        commands.put("/switchcargo", params -> {});
+
+        commands.put("/ejectpeople", params -> {});
+
+        commands.put("/giveup", params -> {});
+
+        commands.put("/viewinventory", params -> {});
+
     }
 }
