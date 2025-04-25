15f02a5c457758d54b10f101fd8f6089cbf8e15e
Nicolò Pallavicini <nicolo.pallavicini@mail.polimi.it>
2025-04-25 18:45:50 +0200

    Almost done with hashmap for messages

--- src/main/java/it/polimi/ingsw/galaxytrucker/Controller/Server/ServerController.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Controller/Server/ServerController.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.galaxytrucker.Controller.Server;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.Game;
-import it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameEvents.HelpEvent;
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameEvents.IllegalEventException;
 import it.polimi.ingsw.galaxytrucker.Model.PlayerShip.Player;
+import it.polimi.ingsw.galaxytrucker.Model.Tiles.Tile;
 
 import java.util.*;
 import java.util.function.Consumer;
@@ -12,35 +12,49 @@ public class ServerController {
     private final Game game;
     private String nickname;
 
-    public ServerController(Map<String, Consumer<List<String>>> commands) {
+    public ServerController(Map<String, Consumer<List<String>>> commands, Game game) {
+
+        this.game = game;
         this.commands = new HashMap<>();
 
         commands.put("/help", params -> {
-            if (!params.isEmpty()){
-                System.out.println("Errore: /help non supporta parametri!");
-                return;
+            try {
+                if (!params.isEmpty()) {
+                    System.out.println("Errore: /help non supporta parametri!");
+                    throw new IllegalEventException("Errore: /help non supporta parametri!");
+                }
+                HelpEvent event = new HelpEvent();
             }
-            HelpEvent event = new HelpEvent();
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
         });
 
         commands.put("/viewleaderboard", params -> {
-            if (!params.isEmpty()){
-                System.out.println("Errore: /viewleaderboard non supporta parametri!");
-                return;
+            try {
+                if (!params.isEmpty()) {
+                    System.out.println("Errore: /viewleaderboard non supporta parametri!");
+                    throw new IllegalEventException("Errore: /viewleaderboard non supporta parametri!");
+                }
+                ViewLeaderboardEvent event = new ViewLeaderboardEvent();
             }
-            ViewLeaderboardEvent event = new ViewLeaderboardEvent();
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
         });
 
         commands.put("/viewships", params -> {
             try {
                 if (!params.isEmpty()) {
                     System.out.println("Errore: /viewships non supporta parametri!");
-                    return;
+                    throw new IllegalEventException("Errore: /viewships non supporta parametri!");
                 }
                 ViewShips event = new ViewShipEvent();
             }
             catch (IllegalEventException e){
-                System.out.println("Errore: " + e.getMessage());
+                System.out.println(e.getMessage());
                 System.err.println(e.getMessage());
             }
         });
@@ -54,17 +68,17 @@ public class ServerController {
                 this.nickname = params.get(0);
                 String IP = params.get(1);
 
-                ArrayList<Player> listOfPlayers = listOfPlayers.getListOfPlayers();
-                ArrayList<String> listOfPlayersString = new ArrayList<String>();
-                for (Player player : listOfPlayers){
-                    String nickPlayer = player.getNickname();
-                    listOfPlayersString.add(nickPlayer);
-                }
-                if (listOfPlayersString.contains(nickname)){
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+
+                if (playerOptional.isPresent()) {
                     System.out.println("Errore: nickname già presente. Sceglierne uno univoco");
                     return;
                 }
+
                 ConnectEvent event = new ConnectEvent(nickname, IP);
+
             }
             catch (IllegalEventException e){
                 System.out.println("Errore: " + e.getMessage());
@@ -78,8 +92,17 @@ public class ServerController {
                     System.out.println("Errore: /disconnect non richiede parametri");
                     throw new IllegalEventException("Errore: /disconnect non richiede parametri");
                 }
+                String nickname = this.nickname;
 
-                DisconnectEvent event = new DisconnectEvent(this.nickname);
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(nickname))
+                        .findFirst();
+
+                Player player = playerOptional.get();
+
+                DisconnectEvent event = new DisconnectEvent(player);
+                System.out.println("Disconnessione avvenuta con successo");
+                // Logic for disconnecting the player with RMI
             }
             catch (IllegalEventException e){
                 System.out.println("Errore: " + e.getMessage());
@@ -87,29 +110,282 @@ public class ServerController {
             }
         });
 
-        commands.put("/setnumberofplayers", params -> {});
+        commands.put("/setnumberofplayers", params -> {
+            try{
+                if (params.size() != 1){
+                    System.out.println("Errore:/setnumberofplayers richiede un parametro");
+                    throw new IllegalEventException("Errore:/setnumberofplayers richiede un parametro");
+                }
+                String numberOfPlayersStr = params.get(0);
+                int numberOfPlayers = Integer.parseInt(numberOfPlayersStr);
 
-        commands.put("/setcolor", params -> {});
+                if (numberOfPlayers < 2 || numberOfPlayers > 4) {
+                    System.out.println("Errore: numero di giocatori non valido. Deve essere compreso tra 2 e 4");
+                    throw new IllegalEventException("Errore: numero di giocatori non valido. Deve essere compreso tra 2 e 4");
+                }
 
-        commands.put("/pickuptile", params -> {});
+                SetNumberOfPlayers event = new SetNumberOfPlayersEvent(numberOfPlayers);
+            }
+            catch (IllegalEventException e){
+                System.out.println("Errore: " + e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
 
-        commands.put("/rotatetile", params -> {});
+        commands.put("/pickuptile", params -> {
+            try{
+                if (params.size() != 2){
+                    System.out.println("Errore: /pickuptile richiede due parametri");
+                    throw new IllegalEventException("Errore: /pickuptile richiede due parametri");
+                }
+                String row = params.get(0);
+                String column = params.get(1);
 
-        commands.put("/putdowntile", params -> {});
+                int rowInt = Integer.parseInt(row);
+                int columnInt = Integer.parseInt(column);
 
-        commands.put("/placetile", params -> {});
+                //change tilepile from list to arraylist?
 
-        commands.put("/reservetile", params -> {});
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
 
-        commands.put("/fliphourglass", params -> {});
+        commands.put("/rotatetile", params -> {
+            try{
+                if (params.size() != 2){
+                    System.out.println("Errore: /rotatetile richiede due parametri");
+                    throw new IllegalEventException("Errore: /rotatetile richiede due parametri");
+                }
+                String row = params.get(0);
+                String column = params.get(1);
 
-        commands.put("/setposition", params -> {});
+                int rowInt = Integer.parseInt(row);
+                int columnInt = Integer.parseInt(column);
 
-        commands.put("/pickupfromship", params -> {});
+                if (rowInt < 5 || rowInt > 9 || columnInt < 4 || columnInt > 10) {
+                    System.out.println("Errore: riga o colonna non valida");
+                    throw new IllegalEventException("Errore: riga o colonna non valida");
+                }
 
-        commands.put("/pickupreservedtile", params -> {});
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if(playerOptional.isEmpty()){
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
 
-        commands.put("/acrivateengine", params -> {});
+                RotateTileEvent event = new RotateTileEvent(player, rowInt, columnInt);
+
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/putdowntile", params -> {
+            try{
+                if (params.size != 4){
+                    System.out.println("Errore: /putdowntile richiede quattro parametri");
+                    throw new IllegalEventException("Errore: /putdowntile richiede quattro parametri");
+                }
+                String row = params.get(0);
+                String column = params.get(1);
+
+                int rowInt = Integer.parseInt(row);
+                int columnInt = Integer.parseInt(column);
+
+                //logic for pile
+
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if(playerOptional.isEmpty()){
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+
+                PutDownTileEvent event = new PutDownTileEvent(player, rowInt, columnInt);
+
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/placetile", params -> {
+            try {
+                if (params.size() != 4) {
+                    System.out.println("Errore: /placetile richiede quattro parametri");
+                    throw new IllegalEventException("Errore: /placetile richiede quattro parametri");
+                }
+                String rowPile = params.get(0);
+                String columnPile = params.get(1);
+                String rowShip = params.get(2);
+                String columnShip = params.get(3);
+
+                int rowPileInt = Integer.parseInt(rowPile);
+                int columnPileInt = Integer.parseInt(columnPile);
+                int rowShipInt = Integer.parseInt(rowShip);
+                int columnShipInt = Integer.parseInt(columnShip);
+
+                if (rowShipInt < 5 || rowShipInt > 9 || columnShipInt < 4 || columnShipInt > 10) {
+                    System.out.println("Errore: riga o colonna non valida");
+                    throw new IllegalEventException("Errore: riga o colonna non valida");
+                }
+
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+
+                if (playerOptional.isEmpty()) {
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
+                PlaceTileEvent event = new PlaceTileEvent(player, rowPileInt, columnPileInt, rowShipInt, columnShipInt);
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/reservetile", params -> {
+            try {
+                if (params.size() != 2) {
+                    System.out.println("Errore: /reservetile richiede due parametri");
+                    throw new IllegalEventException("Errore: /reservetile richiede due parametri");
+                }
+                String row = params.get(0);
+                String column = params.get(1);
+                int rowInt = Integer.parseInt(row);
+                int columnInt = Integer.parseInt(column);
+
+                //logic for pile
+
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if (playerOptional.isEmpty()) {
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
+                ArrayList<Tile> reservedTiles = (ArrayList<Tile>) player.getShip().getReservedTiles(); //In ship maybe return arrayList
+                if (reservedTiles.size() == 2) {
+                    System.out.println("Errore: Non è presente più spazio per tenere da parte la tile, scartarne una prima di chiamare /reservedtile");
+                    throw new IllegalEventException("Errore: Non è presente più spazio per tenere da parte la tile, scartarne una prima di chiamare /reservedtile");
+                }
+                ReserveTileEvent event = new ReserveTileEvent(player, rowInt, columnInt);
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/fliphourglass", params -> {
+            try {
+                if (!params.isEmpty()) {
+                    System.out.println("Errore: /fliphourglass non supporta parametri!");
+                    throw new IllegalEventException("Errore: /fliphourglass non supporta parametri!");
+                }
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if (playerOptional.isEmpty()) {
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
+
+                FlipHourglassEvent event = new FlipHourglassEvent(player);
+            }
+            catch (IllegalEventException e) {
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/setposition", params -> {
+            try{
+                if (params.size() != 1){
+                    System.out.println("Errore: /setposition richiede un parametro");
+                    throw new IllegalEventException("Errore: /setposition richiede un parametro");
+                }
+                String position = params.get(0);
+                int positionInt = Integer.parseInt(position);
+
+                int maxNumberOfPlayers = game.getNumberOfPlayers();
+
+                if (positionInt < 1 || positionInt > maxNumberOfPlayers) {
+                    System.out.println("Errore: posizione non valida. Deve essere compresa tra 1 e 4");
+                    throw new IllegalEventException("Errore: posizione non valida. Deve essere compresa tra 1 e 4");
+                }
+
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if (playerOptional.isEmpty()) {
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
+
+                SetPositionEvent event = new SetPositionEvent(player, positionInt);
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/pickupfromship", params -> {
+            try{
+                if (params.size() != 2){
+                    System.out.println("Errore: /pickupfromship richiede due parametri");
+                    throw new IllegalEventException("Errore: /pickupfromship richiede due parametri");
+                }
+                String row = params.get(0);
+                String column = params.get(1);
+
+                int rowInt = Integer.parseInt(row);
+                int columnInt = Integer.parseInt(column);
+
+                if (rowInt < 5 || rowInt > 9 || columnInt < 4 || columnInt > 10) {
+                    System.out.println("Errore: riga o colonna non valida");
+                    throw new IllegalEventException("Errore: riga o colonna non valida");
+                }
+
+                Optional<Player> playerOptional = game.getListOfPlayers().stream()
+                        .filter(player1 -> player1.getNickname().equals(this.nickname))
+                        .findFirst();
+                if (playerOptional.isEmpty()) {
+                    System.out.println("Errore: giocatore non trovato");
+                    throw new IllegalEventException("Errore: giocatore non trovato");
+                }
+                Player player = playerOptional.get();
+
+                PickupFromShipEvent event = new PickupFromShipEvent(player, rowInt, columnInt);
+            }
+            catch (IllegalEventException e){
+                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
+            }
+        });
+
+        commands.put("/pickupreservedtile", params -> {}); //Really needed?
+
+        commands.put("/acrivateengine", params -> {
+
+        });
 
         commands.put("/activatecannons", params -> {});
 
