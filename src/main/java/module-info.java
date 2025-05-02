43d2fe3a4202c31c53151717f20496f3277a5fd7
elisa <elisa.garsetti@mail.polimi.it>
2025-05-02 16:32:34 +0200

    last bit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module it.polimi.ingsw.galaxytrucker {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.rmi;
 
     opens it.polimi.ingsw.galaxytrucker to javafx.fxml;
     exports it.polimi.ingsw.galaxytrucker;
