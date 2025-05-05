70536601e2e18af33d9078db3c2f16c08b2fa6f3
Luca Papiro <luca.papiro10@gmail.com>
2025-05-05 15:51:25 +0200

    fix

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/GameEvents/ConnectEvent.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/GameEvents/ConnectEvent.java
@@ -2,5 +2,5 @@ package it.polimi.ingsw.galaxytrucker.Model.GamePackage.GameEvents;
 
 import it.polimi.ingsw.galaxytrucker.Model.GamePackage.Game;
 
-public record ConnectEvent(Game game, String nickname, String IP) implements GameEvent {
+public record ConnectEvent(String nickname, String IP) implements GameEvent {
 }
