68aed73b521f7c74ca6505a6a82a38df3e275669
elisa <elisa.garsetti@mail.polimi.it>
2025-05-17 11:52:17 +0200

    gui stuff, going insane

---
+++ src/main/java/it/polimi/ingsw/galaxytrucker/View/GUIFolder/GUITrials.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.galaxytrucker.View.GUIFolder;
+
+public class GUITrials {
+    public GUITrials() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(GUITrials.class.getResource("/images/cardboard/cardboard-1b.jpg"));
+    }
+}
