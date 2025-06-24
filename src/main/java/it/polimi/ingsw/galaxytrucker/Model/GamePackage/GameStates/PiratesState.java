0f26a6edbc84a8acb37b1b9c8b23ec9fdff8f62a
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-06-24 10:43:05 +0200

    little changes PiratesState

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/GameStates/PiratesState.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/GameStates/PiratesState.java
@@ -43,6 +43,7 @@ public class PiratesState extends TravellingState implements Serializable {
         super.init();
         defeatedPlayers = new ArrayList<>();
         defendedPlayers = new ArrayList<>();
+        playersWithIllegalShips = new ArrayList<>();
         game.notifyObservers(game, "pirates");
     }
 
@@ -190,7 +191,7 @@ public class PiratesState extends TravellingState implements Serializable {
         if(reckoningPhase){
             throw new IllegalEventException("Not time for activating shield");
         }
-        else if(defeatedPlayers.contains(event.player()) || defendedPlayers.contains(event.player())){
+        else if(!defeatedPlayers.contains(event.player()) || defendedPlayers.contains(event.player())){
             throw new IllegalEventException("you shall not defend");
         }
         Optional<Tile> tile = event.player().getShip().getTileOnFloorPlan(event.shieldRow(), event.shieldCol());
