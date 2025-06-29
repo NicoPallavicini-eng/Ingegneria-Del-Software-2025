65790a608ceac2ca6a55c06badb49372a574d6fd
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-06-29 17:27:06 +0200

    Added documentation to Game,Hourglass

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/Game.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/GamePackage/Game.java
@@ -17,6 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Game Class unite different Classes,such as Players,GameDeck,TilePile,GameState,Hourglass
+ */
 public class Game implements Serializable, Observable {
     private final List<Observer> observers = new ArrayList<>();
     private final List<Player> listOfActivePlayers = new ArrayList<>();
@@ -29,6 +32,9 @@ public class Game implements Serializable, Observable {
     private final int lapLenght = 24;
     private List<CabinTile> mainCabins = new ArrayList<>();
 
+    /**
+     * Constructor of the Game creates Deck,Cards
+     */
     public Game (){
         JsonParsing jsonTile = new JsonParsing();
         tilePile = new TilePile(jsonTile.getCompleteList()); // shuffle is now automated in TilePile class
@@ -37,18 +43,33 @@ public class Game implements Serializable, Observable {
         deck = new Deck(jsonCardParsing.getCompleteListLevel1(), jsonCardParsing.getCompleteListLevel2());
     }
 
+    /**
+     * This function returns main Cabins
+     * @return List<CabinTile> getMainCabins
+     */
     public List<CabinTile> getMainCabins() {
         return mainCabins;
     }
 
+    /**
+     * This function return a List of players that were connected once to the Game
+     * @return List<Player>
+     */
     public List<Player> getListOfPlayers() {
         return listOfPlayers;
     }
 
+    /**
+     * This function returns a List of Players that are online
+     * @return List<Player>
+     */
     public List<Player> getListOfActivePlayers() {
         return listOfActivePlayers;
     }
 
+    /**
+     * This function updates A list of Active Players
+     */
     public void updateListOfActivePlayers() {
         synchronized (this.listOfActivePlayers) {
             List<Player> list = listOfPlayers.stream().filter(p -> p.getShip().getTravelDays() != null && p.getOnlineStatus()).toList();
@@ -57,20 +78,35 @@ public class Game implements Serializable, Observable {
         }
     }
 
+    /**
+     * this function adds Player to a list of Players,and in the list of Active Players
+     * @param newPlayer
+     */
     public void addPlayer(Player newPlayer){
         listOfPlayers.add(newPlayer);
         listOfActivePlayers.add(newPlayer);
         notifyObservers(this, "game");
     }
 
+    /**
+     * This function return the number of players
+     * @return int
+     */
     public int getNumberOfPlayers() {
         return numberOfPlayers;
     }
 
+    /**
+     * This fuction sets the number of players
+     * @param numberOfPlayers
+     */
     public void setNumberOfPlayers(int numberOfPlayers) {
         this.numberOfPlayers = numberOfPlayers;
     }
 
+    /**
+     * This function sort the list of active Players by Travel Days
+     */
     public void sortListOfActivePlayers(){
         synchronized (listOfActivePlayers) {
             if(listOfActivePlayers.isEmpty()){
@@ -80,50 +116,95 @@ public class Game implements Serializable, Observable {
         }
     }
 
+    /**
+     * This function removes Player from Active List
+     * @param player
+     */
     public void removePlayerFromActiveList(Player player){
         if(listOfActivePlayers.contains(player)){
             listOfActivePlayers.remove(player);
         }
     }
 
+    /**
+     * This function returns actual GameState
+     * @return GameState
+     */
     public GameState getGameState() {
         return gameState;
     }
 
+    /**
+     * This function sets a new Game State of Game
+     * @param gameState
+     */
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
 
+    /**
+     * This function return a Hourglass
+     * @return Hourglass
+     */
     public Hourglass getHourglass() {
         return hourglass;
     }
 
+    /**
+     * This function return a TilePile
+     * @return TilePile
+     */
     public TilePile getTilePile() {
         return tilePile;
     }
 
+    /**
+     * This function return Deck
+     * @return Deck
+     */
     public Deck getDeck() {
         return deck;
     }
 
+    /**
+     * This function returns a size of lapLength of the Board
+     * @return int
+     */
     public int getLapLenght() {
         return lapLenght;
     }
 
+    /**
+     * This function sets hourgllas Parameter of Game
+     * @param hourglass
+     */
     public void setHourglass(Hourglass hourglass) {
         this.hourglass = hourglass;
     }
 
+    /**
+     * This function adds observer to the Game
+     * @param observer
+     */
     @Override
     public void addObserver(Observer observer) {
         observers.add(observer);
     }
 
+    /**
+     * This function removes Observer from Game
+     * @param observer
+     */
     @Override
     public void removeObserver(Observer observer) {
         observers.remove(observer);
     }
 
+    /**
+     * This function notify observers of any change
+     * @param game
+     * @param message
+     */
     @Override
     public void notifyObservers(Game game, String message) {
         for (Observer observer : observers) {
